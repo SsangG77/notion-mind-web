968a3a05eb37469edfb28c1e86ebc7ec2165a284
SsangG77 <sangjincha719@gmail.com>
2026-08-18 18:18:52 +0900

    Initial Commit

---
+++ Mindgraph for Notion/Mindgraph_for_NotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mindgraph_for_NotionApp.swift
+//  Mindgraph for Notion
+//
+//  Created by 차상진 on 8/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Mindgraph_for_NotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
